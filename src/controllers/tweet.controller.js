b04f67a39c423a1d932ba20ed2a62317a6635cea
Yash-Gupta109 <manojkumarilt8@gmail.com>
2024-09-19 22:08:38 +0530

    tweet controller done with all testings

--- src/controllers/tweet.controller.js
+++ src/controllers/tweet.controller.js
@@ -26,15 +26,56 @@ const createTweet = asyncHandler(async (req, res) => {
 
 const getUserTweets = asyncHandler(async (req, res) => {
     // TODO: get user tweets
+    const {userId} = req.params
+    if(!userId?.trim() || !isValidObjectId(userId)){
+        throw new ApiError(404, "Not get userId in getUserTweets")
+    }
+    const tweets =await Tweet.find({owner: userId})
+    
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200, tweets, "User's all tweets fetched successfully")
+    )
     
 })
 
 const updateTweet = asyncHandler(async (req, res) => {
     //TODO: update tweet
+    const {tweetId} = req.params
+    const {content} = req.body
+    if(!tweetId?.trim() || !isValidObjectId(tweetId)){
+        throw new ApiError(404, "tweetId not found in updateTweet")
+    }
+    const updatedTweet = await Tweet.findOneAndUpdate(
+        {_id: tweetId},
+        {
+            $set: {
+                content
+            }
+        },
+        {new: true}
+    ).select("-password")
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200, updatedTweet, "Tweet updated successfully")
+    )
 })
 
 const deleteTweet = asyncHandler(async (req, res) => {
     //TODO: delete tweet
+    const {tweetId} = req.params
+    if(!tweetId?.trim() || !isValidObjectId(tweetId)){
+        throw new ApiError(404, "tweetId not found in deleteTweet")
+    }
+    await Tweet.findByIdAndDelete({_id: tweetId})
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200, {}, "Tweet deleted successfully")
+    );
 })
 
 export {
